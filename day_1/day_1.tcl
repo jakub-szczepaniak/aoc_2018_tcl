6aa1e5714aae0136669e427ef3836474423dce4e
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-03 22:55:50 +0100

    solution to day 1, part 2

--- day_1/day_1.tcl
+++ day_1/day_1.tcl
@@ -12,5 +12,23 @@ proc parse_input { file_content} {
 set puzzle [load_input "input.txt"]
 
 set parsed [parse_input $puzzle]
-puts [expr $parsed]
-#puts $puzzle
+# part one
+
+puts "result part1 [expr $parsed]"
+
+set frequencies [dict create]
+set freq_count [expr [llength $parsed]]
+
+set sum 0
+set ctr 0
+while {![dict exists $frequencies $sum]} {
+		dict append frequencies $sum $sum
+		set sum [expr $sum[lindex $parsed $ctr]]
+		incr ctr
+		if { [expr $ctr > $freq_count -1] } {
+	 		set ctr 0
+  	}
+}
+ puts "result part2 $sum"
+
+
