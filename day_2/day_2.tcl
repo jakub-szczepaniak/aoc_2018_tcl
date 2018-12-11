ec15b35a9a19d0832b9008db2c36bea841dd78cd
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-11 19:04:09 +0100

    we are now counting doubles in box IDs

--- day_2/day_2.tcl
+++ day_2/day_2.tcl
@@ -6,6 +6,13 @@ proc sort_all_items { box_ids } {
 		lappend result [join [lsort [split $box_id ""]]]
 	}
 }
+proc occurences { box_id } {
+	set occurences [dict create]
+	foreach letter [split $box_id ""] {
+		dict incr occurences $letter 1 
+		}
+	return $occurences
+}
 
 proc has_double { box_id } {
 	set box_occ_count [dict values [occurences $box_id] 2]
@@ -17,18 +24,22 @@ proc has_triple { box_id } {
 	return [expr [llength $box_occ_count] ne 0]
 }
 
+proc doubles { box_ids } {
+	set count 0
+	foreach box_id $box_ids {
+		if {[has_double $box_id]} {
+			incr count
+		}
+	}
+	return $count
+}
+
+
+
 set puzzle [load_input "input.txt"]
 
 set parsed [parse_input $puzzle]
 
-set sorted [sort_all_items $parsed]
 
-proc occurences { box_id } {
-	set occurences [dict create]
-	foreach letter [split $box_id ""] {
-		dict incr occurences $letter 1 
-		}
-	return $occurences
-}
 
 #puts $sorted 
