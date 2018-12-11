244ac359e3ff2da86afec29d0032356de51935c7
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-11 15:47:54 +0100

    additional case when there are more than one triple in the string

--- day_2/day_2.tcl
+++ day_2/day_2.tcl
@@ -8,11 +8,11 @@ proc sort_all_items { box_ids } {
 }
 
 proc has_double { box_id } {
-	return [expr [regexp -all (.)\\1 $box_id] eq 1]
+	return [expr [regexp -all (.)\\1 $box_id] >= 1]
 }
 
 proc has_triple { box_id } {
- 	return [expr [regexp -all (.)\\1\\1 $box_id] eq 1]
+ 	return [expr [regexp -all (.)\\1\\1 $box_id] >= 1]
 }
 
 set puzzle [load_input "input.txt"]
