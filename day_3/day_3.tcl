ab8036d4a220cbb1059c3d4c7984f8a99a10c5e4
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-13 13:34:42 +0100

    tests to verify calculating covered coordinates works

--- day_3/day_3.tcl
+++ day_3/day_3.tcl
@@ -17,9 +17,9 @@ proc list_covered { claim } {
 	set height [dict get $claim h]
 	set result [list]
 	
-	for {set counter_x $top_corner_x} { $counter_x < [expr $width+$top_corner_x]} {incr counter_x} {
+	for {set counter_x $top_corner_x} { $counter_x < [expr $width + $top_corner_x]} {incr counter_x} {
 		for {set counter_y $top_corner_y} {$counter_y < [expr $height + $top_corner_y]} {incr counter_y} {
-		lappend result "$counter_x:$counter_y" 
+		lappend result "$counter_x:$counter_y"
 		}
 	}
 	return $result
@@ -33,4 +33,4 @@ set claims [list]
 foreach line $lines {
 	lappend claims [as_claim $line]
 } 
-puts $claims
+#puts $claims
