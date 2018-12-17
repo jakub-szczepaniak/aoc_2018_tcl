cc6db731efa77f4f23ee823a8e0a5a17fc40fef0
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-17 21:09:13 +0100

    calculating the times of sleeping for guards

--- day_4/day_4.tcl
+++ day_4/day_4.tcl
@@ -21,13 +21,27 @@ foreach log_entry $sorted {
 			dict append entries_by_guard $current_guard_id [list] 
 			 }
 		"\\[.+?:(\\d{2})\\] wakes up" { 
-			dict lappend entries_by_guard $current_guard_id [list 1 [lindex $groups 1]]
+			dict lappend entries_by_guard $current_guard_id [list 1 [to_int [lindex $groups 1]]]
 		}
 		"\\[.+?:(\\d{2})\\] falls asleep" { 
-			dict lappend entries_by_guard $current_guard_id [list 0 [lindex $groups 1]]
+			dict lappend entries_by_guard $current_guard_id [list 0 [to_int [lindex $groups 1]]]
 			}
 		default { puts "Parsing didnt match"}				
 		}
 	}
 	puts $entries_by_guard
+
+set times_by_guard [dict create ]
+
+dict for {guard_id commands} $entries_by_guard  {
+	puts $guard_id
+	set time_slept 0
+	foreach {start_command end_command} $commands {
+		set start_time [lindex $start_command 1]
+		set end_time [lindex $end_command 1]
+		set time_slept [expr ($end_time - $start_time)]
+	}
+	puts $time_slept
+	
+}
 
