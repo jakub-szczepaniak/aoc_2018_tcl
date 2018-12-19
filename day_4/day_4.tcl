6fd03207095895703c716674270a3fda54966697
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-19 20:52:50 +0100

    different, simpler approach

--- day_4/day_4.tcl
+++ day_4/day_4.tcl
@@ -23,59 +23,31 @@ set parsed [parse_input $puzzle]
 set sorted [lsort $parsed]
 save_output "sorted.txt" $sorted
 
-set entries_by_guard [dict create]
+set time_by_guard [dict create]
+
 set current_guard_id 0
 foreach log_entry $sorted {
 	set command [parse_command $log_entry]
-	switch -regexp -matchvar groups -- $log_entry {
-		"Guard #(\\d+?) begins shift" { 
-			set current_guard_id [lindex $groups 1]
-			if {[dict exists $entries_by_guard $current_guard_id]} {
-				continue
-			}
-			dict append entries_by_guard $current_guard_id [list] 
-			 }
-		"\\[.+?:(\\d{2})\\] wakes up" { 
-			dict lappend entries_by_guard $current_guard_id [list 1 [to_int [lindex $groups 1]]]
-		}
-		"\\[.+?:(\\d{2})\\] falls asleep" { 
-			dict lappend entries_by_guard $current_guard_id [list 0 [to_int [lindex $groups 1]]]
-			}
-		default { puts "Parsing didnt match"}				
-		}
-	}
-
-set times_by_guard [dict create ]
-
-dict for {guard_id commands} $entries_by_guard  {
 	set time_slept 0
-	foreach {start_command end_command} $commands {
-		set start_time [lindex $start_command 1]
-		set end_time [lindex $end_command 1]
-		set time_slept [expr ($end_time - $start_time)]
+	switch [lindex $command 0] {
+		 "guard_id" {
+			set current_guard_id [lindex $command 1]
+		}
+		"asleep" {
+			set start_minute [lindex $command 1] 
+		}
+		"awake" {
+			set end_minute [lindex $command 1]
+			set time_slept [expr $end_minute - $start_minute]
+			if {[dict exists time_by_guard $current_guard_id]} {
+					set curr_time [dict get time_by_guard $current_guard_id]
+					dict set time_by_guard $current_guard_id [expr $time_slept + $curr_time] 
+				} else {
+				dict set time_by_guard $current_guard_id $time_slept
+			}
+		}
 	}
-	dict append times_by_guard $guard_id $time_slept
-}
-
-set max_guard [max_value_key $times_by_guard]
-puts "Guard id : $max_guard"
-
-set entries_max_guard [dict get $entries_by_guard $max_guard]
-
-set minutes [dict create]
-
-for { set x 0 } { $x < 60} { incr x} {
-	dict append minutes $x 0
-}
-
-foreach { start end } $entries_max_guard {
-	for { set x [lindex $start 1]} { $x <= [lindex $end 1] } { incr x} {
-		dict incr minutes $x
+	
 	}
-}
-puts $minutes
-puts [max_value_key $minutes]
-
-
-puts [expr [max_value_key $minutes]*$max_guard]
+puts [max_value_key $time_by_guard]
 
