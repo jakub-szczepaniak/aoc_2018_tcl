486e12dbc37790860e25e21f2afca434232803cf
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-21 22:00:55 +0100

    parsing input for day 6

--- day_6/day_6.tcl
+++ day_6/day_6.tcl
@@ -1,1 +1,19 @@
 source [file join [file dirname [info script]] ../utils utils.tcl]
+
+proc parse_coordinates { file_content } {
+	set lines [parse_input $file_content]
+	set coordinates [list]
+	foreach line $lines {
+		scan $line {%d, %d} x y
+		lappend coordinates $x $y
+	}
+	return $coordinates
+}
+
+set puzzle_input [load_input "input.txt"]
+
+set coordinates [parse_coordinates $puzzle_input]
+
+puts $coordinates
+
+
