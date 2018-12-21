eafcab963f1316738a2875edc121e1bb06ffba5d
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-21 22:17:11 +0100

    calculate size of the matrix + manhattan distance caclulation

--- day_6/day_6.tcl
+++ day_6/day_6.tcl
@@ -10,9 +10,30 @@ proc parse_coordinates { file_content } {
 	return $coordinates
 }
 
+proc min_max_coordinates { coordinates } {
+	set max_x 0
+	set max_y 0
+	set min_x 999
+	set min_y 999
+	foreach { x y } $coordinates {
+		set min_x [ expr $x <= $min_x ? $x : $min_x]
+		set min_y [ expr $y <= $min_y ? $y : $min_y]
+		set max_x [ expr $x >= $max_x ? $x : $max_x]
+		set max_y [ expr $y >= $max_y ? $y : $max_y]
+	}
+	return [list $min_x $min_y $max_x $max_y]
+}
+
+proc manhattan_dist { x1 y1 x2 y2 } {
+	return [expr {abs($x2-$x1) + abs($y2-$y1)}]
+}
+
 set puzzle_input [load_input "input.txt"]
 
 set coordinates [parse_coordinates $puzzle_input]
+
+puts [min_max_coordinates $coordinates]
+
 
 puts $coordinates
 
