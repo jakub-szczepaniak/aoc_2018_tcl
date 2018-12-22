a28ba04e24c092754947b0106b1a9ea0072cf4fb
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-22 22:21:42 +0100

    day 6, part 2 completed

--- day_6/day_6.tcl
+++ day_6/day_6.tcl
@@ -49,9 +49,10 @@ for {set dx [lindex $min_max 0]} { $dx < [lindex $min_max 2] } {incr dx} {
 			foreach {id x y} $coordinates {
 				set distance [manhattan_dist $x $y $dx $dy]
 				if { ![dict exists $distances "$dx:$dy"]} {
-					dict set distances "$dx:$dy" [list $id $distance]
+					dict set distances "$dx:$dy" [list $id $distance $distance]
 				} else {
 					set current [lindex [dict get $distances "$dx:$dy"] 1]
+					
 					if {$current > $distance} {
 						dict set distances "$dx:$dy" [list $id $distance]
 					}
@@ -78,4 +79,23 @@ foreach inf [lsort -unique $infinite] {
 	dict unset areas $inf
 }
 puts "part 1 : [dict get $areas [max_value_key $areas]]"
+
+# part 2
+
+set area 0
+
+set distances [dict create]
+
+for {set dx [lindex $min_max 0]} { $dx < [lindex $min_max 2] } {incr dx} {
+	for {set dy [lindex $min_max 1]} { $dy < [lindex $min_max 3] } {incr dy} {
+			foreach {id x y} $coordinates {
+				set distance [manhattan_dist $x $y $dx $dy]
+				dict incr distances "$dx:$dy" $distance
+			}
+			if { [dict get $distances "$dx:$dy"] < 10000} {
+				incr area
+			}
+	}
+}
+puts "Part 2: $area"
 
