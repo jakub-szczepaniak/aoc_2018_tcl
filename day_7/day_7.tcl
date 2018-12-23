41b84207a4313cedb600faef1e8cab9caa643b09
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-23 21:37:54 +0100

    implementation with the stack

--- day_7/day_7.tcl
+++ day_7/day_7.tcl
@@ -1,16 +1,34 @@
 source [file join [file dirname [info script]] ../utils utils.tcl]
 
+package require struct
+
 
 set puzzle_input [parse_input [load_input "input.txt"]]
 
-set steps [ dict create ]
+set dependencies [ dict create ]
+set steps [list]
 foreach step $puzzle_input {
 	scan $step "Step %s must be finished before step %s" pred succ
-	dict lappend steps $pred $succ
-	dict set steps $pred [lsort [dict get $steps $pred]]
+	dict lappend dependencies $pred $succ
+	dict set dependencies $pred [lsort [dict get $dependencies $pred]]
+	lappend steps $pred
+	lappend steps $succ
 } 
 
+set steps [lsort -unique $steps]
 
-dict for { pred succ } $steps {
+puts $steps
+
+dict for { pred succ } $dependencies {
 	puts "$pred => $succ"
 }
+
+set finished [list]
+
+::struct::stack processing
+
+processing push [lindex $steps 0]
+puts [processing peek]
+
+
+
