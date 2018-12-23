a32d4372d8ff92112ed0ac3b1630bdbca2042eb4
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-23 20:31:33 +0100

    parsing and sorting input

--- day_7/day_7.tcl
+++ day_7/day_7.tcl
@@ -1,1 +1,16 @@
 source [file join [file dirname [info script]] ../utils utils.tcl]
+
+
+set puzzle_input [parse_input [load_input "input.txt"]]
+
+set steps [ dict create ]
+foreach step $puzzle_input {
+	scan $step "Step %s must be finished before step %s" pred succ
+	dict lappend steps $pred $succ
+	dict set steps $pred [lsort [dict get $steps $pred]]
+} 
+
+
+dict for { pred succ } $steps {
+	puts "$pred => $succ"
+}
