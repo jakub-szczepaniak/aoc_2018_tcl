db0f51047c97ccb906226534667e23ecc1397877
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2018-12-27 21:59:34 +0100

    wrap queue in their methods

--- day_7/day_7.tcl
+++ day_7/day_7.tcl
@@ -12,7 +12,7 @@ proc all_in { list1 list2 } {
 	return $result
 }
 
-proc succesors_for { predecessor } {
+proc successors_for { predecessor } {
 	global successors
 	return [dict get $successors $predecessor]
 }
@@ -32,35 +32,63 @@ proc add_predecessor_for { successor predecessor} {
 	dict lappend predecessors $successor $predecessor
 }
 
+proc prepare_queue {} { 
+	::struct::prioqueue -ascii processing
+}
+
+proc queue_push { element } {
+	processing put $element $element
+}
+
+proc queue_pop {} {
+	return [processing get] 
+}
+
+proc queue_size {} {
+	return [processing size]
+}
+
+proc queue_peek {} {
+	return [processing peek]
+}
+
+proc destroy_queue {} {
+	processing destroy
+}
 
 set puzzle_input [parse_input [load_input "input.txt"]]
 
 set successors [dict create]
+
 set predecessors [dict create]
 
-set steps [list]
 foreach step $puzzle_input {
 	scan $step "Step %s must be finished before step %s" pred succ
 	add_successor_for $pred $succ
 	add_predecessor_for $succ $pred
 } 
 
-puts [dict keys $successors]
-puts [dict keys $predecessors]
+puts [lsort [dict keys $successors]]
+puts [lsort [dict keys $predecessors]]
 
-::struct::prioqueue -ascii processing
+prepare_queue
 
-foreach element [dict keys $predecessors] {
-	if { [not_in [dict keys $successors] $element ]} {
-		processing put $element $element
+foreach element [dict keys $successors] {
+	if { [not_in [dict keys $predecessors] $element ]} {
+		puts $element
+		queue_push $element
 	}
 }
 
-puts [processing size] 
-puts [processing peek]
 set finished [list]
 
-while { [processing size]} {
+while {[queue_size]} {
+	set next_action [queue_peek]
+	if { [all_in [ predecessors_for $next_action] $finished] } {
+		lappend $finished [queue_pop]
+		continue
+	}
+	break
 }
 puts $finished
 	
