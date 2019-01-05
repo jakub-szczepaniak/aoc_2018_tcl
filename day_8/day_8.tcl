c3bc9800ec601dc9012b6df7cb7d55f6c89dbcd4
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2019-01-05 23:17:35 +0100

    parsing input into a list of numbers

--- day_8/day_8.tcl
+++ day_8/day_8.tcl
@@ -1,1 +1,15 @@
 source [file join [file dirname [info script]] ../utils utils.tcl]
+
+proc prepare_input {} {
+	set puzzle_input [split [load_input "input.txt"] ""]
+
+	set parsed [flatten $puzzle_input]
+
+	set converted_to_int [list]
+	foreach item $parsed {
+		lappend converted_to_int [to_int $item]
+	}
+	return $converted_to_int
+}
+set puzzle_input [prepare_input]
+puts $puzzle_input
