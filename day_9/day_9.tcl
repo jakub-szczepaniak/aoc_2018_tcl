4a2f4f32ec83860d5ca6159c0eee21a7aeb82b61
Jakub Szczepaniak <jakub.szczepaniak@milengo.com>
2019-03-04 22:16:01 +0100

    we can add first marble

--- day_9/day_9.tcl
+++ day_9/day_9.tcl
@@ -1,3 +1,11 @@
 proc init_game {} {
 	return [list 0 0]
 }
+
+proc add_marble {game_field marble} {
+	set field [lassign $game_field position]
+	
+	set field [lappend field $marble]
+	set field [lappend field 1]
+	return $field
+}
